bca78729f2ad1ae0b59a9c08fbb47927459d8640
William Friesen <wfriesen@gmail.com>
2010-08-16 07:25:05 +1000

    qwantz.com: Now also shows the RSS title

--- qwantz.js
+++ qwantz.js
@@ -4,8 +4,8 @@ var title = info[0];
 var image = info[1];
 
 //subject line of "contact" email address
-var subject = ""
-links = document.getElementsByTagName("a")
+var subject = "";
+links = document.getElementsByTagName("a");
 for (i=0; i<links.length; i++) {
 	if (links[i].innerHTML == "contact") {
 		var contact = links[i].getAttribute("href");
@@ -15,6 +15,13 @@ for (i=0; i<links.length; i++) {
 	}
 }
 
+//RSS title
+var rss = "";
+var txt = new RegExp("<span class=\"rss-title\">(.*?)</span>","m");
+if (txt.test(document.documentElement.outerHTML)) {
+	rss = txt.exec(document.documentElement.outerHTML)[1];
+}
+
 var div = document.createElement("div");
 var titleElement = document.createElement("p");
 titleElement.innerHTML = title;
@@ -22,4 +29,7 @@ div.appendChild(titleElement);
 var subjectElement = document.createElement("p");
 subjectElement.innerHTML = subject;
 div.appendChild(subjectElement);
+var rssElement = document.createElement("p");
+rssElement.innerHTML = rss;
+div.appendChild(rssElement);
 image.parentNode.insertBefore(div,image.nextSibling);
