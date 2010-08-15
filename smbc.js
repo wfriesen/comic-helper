35a708b0bc8dff0077c02757c9e68fad89c8e9d9
William Friesen <wfriesen@gmail.com>
2010-08-15 17:52:07 +1000

    Display the smbc votey just above the news. Only insert it if the link has been found

--- smbc.js
+++ smbc.js
@@ -1,13 +1,17 @@
 for (i=0; i<document.images.length; i++) {
 	if (document.images[i].getAttribute("src").indexOf("after.gif") != -1) {
 		var votey = document.images[i].getAttribute("src");
-		break;
-	}
-}
 
-for (i=0; i<document.getElementsByTagName("img").length; i++) {
-	if (document.getElementsByTagName("img")[i].src.indexOf("secretvoteybutton.gif") != -1) {
-		document.getElementsByTagName("img")[i].src = votey;
+		for (i=0; i<document.getElementsByTagName("img").length; i++) {
+			if (document.getElementsByTagName("img")[i].src.indexOf("secretvoteybutton.gif") != -1) {
+				var e = document.getElementsByTagName("img")[i];
+				while (e.tagName.toLowerCase() != "table") e = e.parentNode;
+				e = e.parentNode;
+				var voteyElement = document.createElement("img");
+				voteyElement.setAttribute("src",votey);
+				e.insertBefore(voteyElement);
+			}
+		}
 		break;
 	}
 }
