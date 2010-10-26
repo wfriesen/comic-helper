9e5fc2416268c0392e71c008d086a14325af0b09
William Friesen <wfriesen@gmail.com>
2010-10-26 18:49:26 +1100

    Basic caching for secrets for ASP

--- appengine/comic-helper.py
+++ appengine/comic-helper.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import webapp
+from google.appengine.ext import db
 from google.appengine.ext.webapp.util import run_wsgi_app
 import urllib2
 from urlparse import urljoin
@@ -12,22 +13,42 @@ def get_html(url):
 		return None
 	return handle.read()
 
+class SecretModel(db.Model):
+	link = db.StringProperty()
+	secret = db.StringProperty()
+	date = db.DateTimeProperty(auto_now_add=True)
+
+def get_secrets(link):
+	cache = db.GqlQuery("SELECT * FROM SecretModel WHERE link='"+link+"' LIMIT 1")
+	if cache.count() == 1:
+		return cache[0].secret
+
+	html = get_html(link)
+	if not html:
+		return None
+
+	soup = BeautifulSoup(html)
+	secret = None
+	for img in soup.findAll("img"):
+		if "ASPeasteregg.png" in img["src"]:
+			try:
+				secret = urljoin(link,img.parent["href"])
+			except KeyError:
+				pass
+	new_secret = SecretModel()
+	new_secret.link = link
+	new_secret.secret = secret
+	new_secret.put()
+
+	return secret
+
 class ASP(webapp.RequestHandler):
 	def get(self):
 		link = self.request.get("link")
-		html = get_html(link)
-		if not html:
-			self.response.out.write(json.dumps({}))
-			return
+
+		secret = get_secrets(link)
+
 		self.response.headers["Access-Control-Allow-Origin"] = "http://www.google.com"
-		soup = BeautifulSoup(html)
-		secret = None
-		for img in soup.findAll("img"):
-			if "ASPeasteregg.png" in img["src"]:
-				try:
-					secret = urljoin(link,img.parent["href"])
-				except KeyError:
-					pass
 		if secret:
 			self.response.out.write(json.dumps({"panel":secret}))
 		else:
